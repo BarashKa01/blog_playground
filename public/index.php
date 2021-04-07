8f01afe10e9686a5647fd21b755411106f278272
Cyrille Nicaise <cyrille.ni1@gmail.com>
2021-04-07 08:26:21 +0200

    homepage simple init

--- public/index.php
+++ public/index.php
@@ -3,3 +3,13 @@
 require '..App//Autoloader.php';
 
 App\Autoloader::register();
+
+if (isset($_GET['page'])) {
+    $page = $_GET['page'];
+} else {
+    $page = 'home';
+}
+
+if ($page === 'home'){
+    require '../pages/home.php';
+}
