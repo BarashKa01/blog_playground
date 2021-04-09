2853856cff966eb9e82b016e88b26a3fb0cbf10b
Cyrille Nicaise <cyrille.ni1@gmail.com>
2021-04-09 08:26:29 +0200

    basic PDO exec

--- pages/home.php
+++ pages/home.php
@@ -1,3 +1,13 @@
-<h1>Hello, welcome to blog playground :)</h1>
+<?php
 
-<p><a href="index.php?page=single">Single</a></p>
+$pdo = new PDO('mysql:dbname=blog_playground;host=localhost', 'root', '');
+//Verbose mode
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$pdo->exec('INSERT INTO article SET title="Title", "date="'. date('Y-ù-d H:i:s'). '"');
+
+$statement = $pdo->query('SELECT * FROM article');
+
+$datas = $statement->fetchAll(PDO::FETCH_OBJ);
+
+var_dump($datas[0]->title);
